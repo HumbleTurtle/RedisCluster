4c95e2fa28fe5af6f3e849c94f0fc1e20128914f
HumbleTurtle <8689879+HumbleTurtle@users.noreply.github.com>
2023-01-11 19:06:09 -0300

    Cambios

--- montar-docker.sh
+++ montar-docker.sh
@@ -2,4 +2,4 @@
 
 docker rm -f redis; \
 docker build -t redis-cluster-local . &&
-docker run -d -p 6379:6379 --net=host --restart unless-stopped redis-cluster-local  --name redis-cluster-local redis-server /usr/local/etc/redis/redis.conf
+docker run -d -p 6379:6379 --net=host --restart unless-stopped --name redis-cluster-local redis-cluster-local redis-server
